aaf22be21d126453c4533667fbed0ad9bc7dc2ef
Louya64 <cixie@live.fr>
2021-12-20 09:41:36 +0100

    update sass structure

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,24 @@
-import '../sass/header.css'
-import Logo from '../../resources/logo.jpg'
+import '../styles.css';
+import Logo from '../../resources/logo.jpg';
 import React from 'react';
 
-
 const Header = () => {
-    return (
-        <div className="header">
-            <img id="logoHeader" src={Logo} alt={Logo} />
-            <ul className="headerContainer">
-                <li><a href="/location">Près de chez vous</a> </li>
-                <li><input id="searchHome" type="search" value='Rechercher un article'/></li>
-                <li><a href="/mon_compte">Mon compte</a> </li>
-            </ul>
-        </div>
-    )
+  return (
+    <div className="header">
+      <img id="logoHeader" src={Logo} alt={Logo} />
+      <ul className="headerContainer">
+        <li>
+          <a href="/location">Près de chez vous</a>{' '}
+        </li>
+        <li>
+          <input id="searchHome" type="search" value="Rechercher un article" />
+        </li>
+        <li>
+          <a href="/mon_compte">Mon compte</a>{' '}
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Header;
