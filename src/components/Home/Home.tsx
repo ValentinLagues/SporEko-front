5016ea27cec6e4a47d908a53026db66cab5914b5
jarod79 <arrijuria.eric@gmail.com>
2022-01-03 15:01:38 +0100

    test bacground color

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,21 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 
+import Logo from '../../../resources/Logo_transparent.png';
 import Loupe from '../../../resources/loupe.png';
 
 const Home = () => {
-  const [search, setSearch] = useState('Off');
-
   return (
     <div className="home">
       <div className="home__Container">
-        <h1>
-          Spor<span>E</span>ko
-        </h1>
+        <img src={Logo} alt="Sporeko" />
+
         <p>Dépensez moins, pour vous dépenser plus!</p>
         <form>
-          <button onClick={() => setSearch('On')}>
-            <img src={Loupe} alt="loupe" />
-          </button>
+          <img src={Loupe} alt="loupe" />
+
           <input className="btn" type="text" placeholder="Recherchez sur SPOREKO" />
         </form>
       </div>
