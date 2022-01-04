cf2f37772673edc9cc14aa9244a72a484ec40818
jarod79 <arrijuria.eric@gmail.com>
2022-01-04 14:19:08 +0100

    Update background color and text color

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import Logo from '../../../resources/Logo_transparent.png';
-import Loupe from '../../../resources/loupe.png';
+import Logo from '../../../resources/Logo_dark.png';
 
 const Home = () => {
   return (
@@ -11,14 +10,6 @@ const Home = () => {
 
         <p>Dépensez moins, pour vous dépenser plus!</p>
         <form>
-<<<<<<< HEAD
-          <img src={Loupe} alt="loupe" />
-
-=======
-          <button onClick={() => console.log('youpi')}>
-            <img src={Loupe} alt="loupe" />
-          </button>
->>>>>>> fcf6002e7d6c7ced089aca3748a4d616bc4cbc0e
           <input className="btn" type="text" placeholder="Recherchez sur SPOREKO" />
         </form>
       </div>
