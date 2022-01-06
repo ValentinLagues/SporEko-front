fe8ac92f46b334bd77d16e5965f4cb278ada0c02
Louya64 <cixie@live.fr>
2022-01-06 19:57:11 +0100

    join byHand and price inpur to state and setState

--- src/components/mobile/offerForm/OfferForm.jsx
+++ src/components/mobile/offerForm/OfferForm.jsx
@@ -1,51 +1,45 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { BsPlusLg } from 'react-icons/bs';
-import { FaRunning } from 'react-icons/fa';
-import { FaWalking } from 'react-icons/fa';
-import { IoFitness } from 'react-icons/io5';
-import { MdDirectionsBike } from 'react-icons/md';
+import { MdStarRate } from 'react-icons/md';
+
+const urlBack = 'http://localhost:8000/';
 
 const OfferForm = () => {
-  const [sportsList, setSportsList] = useState([]);
+  const [sportList, setSportList] = useState([]);
+  const [genderList, setGenderList] = useState([]);
   const [categoryList, setCategoryList] = useState([]);
   const [brandList, setBrandList] = useState([]);
-  const [textileList, setTextileList] = useState([]);
-  const [conditionList, setConditionList] = useState([]);
   const [colorList, setColorList] = useState([]);
+  const [conditionList, setConditionList] = useState([]);
+  const [sizeList, setSizeList] = useState([]);
+  const [colissimoList, setColissimoList] = useState([]);
 
+  const [sport, setSport] = useState('');
+  const [gender, setGender] = useState('');
   const [category, setCategory] = useState('');
   const [brand, setBrand] = useState('');
-  const [textile, setTextile] = useState('');
-  const [condition, setCondition] = useState('');
   const [color, setColor] = useState('');
+  const [condition, setCondition] = useState('');
+  const [price, setPrice] = useState(0);
+  const [size, setSize] = useState('');
+  const [colissimo, setColissimo] = useState('');
+  const [byHand, setByHand] = useState(false);
 
   useEffect(() => {
-    axios.get('http://localhost:7000/sports').then((res) => setSportsList(res.data));
-    axios
-      .get('http://localhost:7000/categories')
-      .then((res) => setCategoryList(res.data));
-    axios.get('http://localhost:7000/brands').then((res) => setBrandList(res.data));
-    axios.get('http://localhost:7000/textiles').then((res) => setTextileList(res.data));
-    axios
-      .get('http://localhost:7000/conditions')
-      .then((res) => setConditionList(res.data));
-    axios.get('http://localhost:7000/colors').then((res) => setColorList(res.data));
+    axios.get(`${urlBack}sports`).then((res) => setSportList(res.data));
+    axios.get(`${urlBack}genders`).then((res) => setGenderList(res.data));
+    axios.get(`${urlBack}categories`).then((res) => setCategoryList(res.data));
+    axios.get(`${urlBack}brands`).then((res) => setBrandList(res.data));
+    axios.get(`${urlBack}colors`).then((res) => setColorList(res.data));
+    axios.get(`${urlBack}conditions`).then((res) => setConditionList(res.data));
+    axios.get(`${urlBack}sizes`).then((res) => setSizeList(res.data));
+    axios.get(`${urlBack}colissimos`).then((res) => setColissimoList(res.data));
   }, []);
 
   return (
     <div className="offerForm">
       <form className="offerForm__form" action="">
-        <div>
-          {sportsList &&
-            sportsList.map((sport, index) => <span key={index}>{sport.name}</span>)}
-        </div>
-        <div className="offerForm__sportIcons">
-          <FaRunning className="offerForm__sportIcons__icon" />
-          <MdDirectionsBike className="offerForm__sportIcons__icon" />
-          <FaWalking className="offerForm__sportIcons__icon" />
-          <IoFitness className="offerForm__sportIcons__icon" />
-        </div>
         <div id="addPhotoContainer">
           <label id="labelPhoto1" htmlFor="photo1">
             <BsPlusLg /> AJOUTER PHOTOS
@@ -54,15 +48,47 @@ const OfferForm = () => {
         </div>
         <div>
           <label className="offerForm__label" htmlFor="title">
-            Titre
+            Titre <MdStarRate className="iconRequired" />
           </label>
           <input className="offerForm__input" type="text" id="title" />
         </div>
         <div>
           <label className="offerForm__label" htmlFor="description">
-            Description
+            Description <MdStarRate className="iconRequired" />
           </label>
           <textarea className="offerForm__input" rows={5} id="description" />
+        </div>
+        <div>
+          <select
+            onChange={(e) => setSport(e.target.value)}
+            value={sport}
+            className="offerForm__select"
+            name="categories"
+            id="categories">
+            <option value="">Sport</option>
+            {sportList &&
+              sportList.map((sport, index) => (
+                <option key={index} value={index + 1}>
+                  {sport.name}
+                </option>
+              ))}
+          </select>
+        </div>
+        <div>
+          <select
+            onChange={(e) => setGender(e.target.value)}
+            value={gender}
+            className="offerForm__select"
+            name="categories"
+            id="categories">
+            <option value="">Genre</option>
+            {genderList &&
+              genderList.map((gender, index) => (
+                <option key={index} value={index + 1}>
+                  {gender.name}
+                </option>
+              ))}
+          </select>
         </div>
         <div>
           <select
@@ -98,32 +124,16 @@ const OfferForm = () => {
         </div>
         <div>
           <select
-            onChange={(e) => setTextile(e.target.value)}
-            value={textile}
+            onChange={(e) => setSize(e.target.value)}
+            value={size}
             className="offerForm__select"
-            name="textile"
-            id="textiles">
-            <option value="">Matière</option>
-            {textileList &&
-              textileList.map((textile, index) => (
+            name="size"
+            id="sizes">
+            <option value="">Taille</option>
+            {sizeList &&
+              sizeList.map((size, index) => (
                 <option key={index} value={index + 1}>
-                  {textile.name}
-                </option>
-              ))}
-          </select>
-        </div>
-        <div>
-          <select
-            onChange={(e) => setCondition(e.target.value)}
-            value={condition}
-            className="offerForm__select"
-            name="condition"
-            id="conditions">
-            <option value="">État</option>
-            {conditionList &&
-              conditionList.map((condition, index) => (
-                <option key={index} value={index + 1}>
-                  {condition.name}
+                  {size.name}
                 </option>
               ))}
           </select>
@@ -143,6 +153,58 @@ const OfferForm = () => {
                 </option>
               ))}
           </select>
+        </div>
+        <div>
+          <select
+            onChange={(e) => setCondition(e.target.value)}
+            value={condition}
+            className="offerForm__select"
+            name="condition"
+            id="conditions">
+            <option value="">État du produit</option>
+            {conditionList &&
+              conditionList.map((condition, index) => (
+                <option key={index} value={index + 1}>
+                  {condition.name}
+                </option>
+              ))}
+          </select>
+        </div>
+        <div>
+          <label className="offerForm__label" htmlFor="price">
+            Prix hors frais de port <MdStarRate className="iconRequired" />
+          </label>
+          <input
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            className="offerForm__input"
+            type="number"
+            step={0.01}
+            id="price"
+          />
+        </div>
+        <div>
+          <select
+            onChange={(e) => setColissimo(e.target.value)}
+            value={colissimo}
+            className="offerForm__select"
+            name="colissimo"
+            id="colissimos">
+            <option value="">Format du produit</option>
+            {colissimoList &&
+              colissimoList.map((colissimo, index) => (
+                <option key={index} value={index + 1}>
+                  {colissimo.name}
+                </option>
+              ))}
+          </select>
+        </div>
+        <div>
+          Autoriser la remise en main propre
+          <label className="switch">
+            <input checked={byHand} onChange={() => setByHand(!byHand)} type="checkbox" />
+            <span className="slider round"></span>
+          </label>
         </div>
       </form>
     </div>
