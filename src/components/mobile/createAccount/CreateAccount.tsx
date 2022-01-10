711be2583048dc89c42ddc91b07ca63b807ebea7
amandine <constans.amandine@gmail.com>
2022-01-10 16:09:07 +0100

    version 2 front page create account

--- src/components/mobile/createAccount/CreateAccount.tsx
+++ src/components/mobile/createAccount/CreateAccount.tsx
@@ -1,218 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { CgProfile } from 'react-icons/cg';
+import { HiEye, HiOutlineUserRemove } from 'react-icons/hi';
+import { ImKey } from 'react-icons/im';
+import { MdOutlineEmail } from 'react-icons/md';
+import { RiLockPasswordLine } from 'react-icons/ri';
 
 const CreateAccount = () => {
+  const [hiEye, setHiEye] = useState(true);
+  // const [hiEyeConfirm, setHiEyeConfirm] = useState(true);
+
   return (
     <div className="create-account">
-      <h2 className="create-account__h2">Créez votre compte</h2>
-      <div className="create-account__h2__border"></div>
-      <div className="create-account__left">
-        <div className="create-account__left__input-radio">
-          <h3>Profil</h3>
-          <input
-            className="create-account__left__input-radio__radio"
-            type="radio"
-            name="profil"
-          />
-          Particulier
-          <input
-            className="create-account__left__input-radio__radio"
-            type="radio"
-            name="profil"
-          />
-          Professionnel
-        </div>
-        <h3>Genre</h3>
-        <input
-          className="create-account__left__input-radio__radio"
-          type="radio"
-          name="genre"
-        />{' '}
-        M
-        <input
-          className="create-account__left__input-radio__radio"
-          type="radio"
-          name="genre"
-        />{' '}
-        Mme
-        <input
-          className="create-account__left__input-radio__radio"
-          type="radio"
-          name="genre"
-        />{' '}
-        Autre
-        <div className="create-account__register">
-          <div className="create-account__register__title1">
-            <label htmlFor="pseudo">Pseudo*</label>
-          </div>
-          <div className="create-account__register__title2">
+      <h2>Créez votre compte</h2>
+      <form className="create-account__form" action="">
+        <div className="create-account__form__inputsContainer">
+          <div className="create-account__form__inputsContainer__input">
+            <HiOutlineUserRemove className="inputIcon" />
             <input
-              className="create-account__register__input"
               name="Pseudo"
               type="text"
               id="pseudo"
-              placeholder="John64"
+              placeholder="Choisissez un pseudo"
             />
           </div>
-        </div>
-        <div className="create-account__register">
-          <div className="create-account__register__title1">
-            <label htmlFor="Prénom">Prénom*</label>
+          <div className="create-account__form__inputsContainer__input">
+            <CgProfile className="inputIcon" />
+            <input name="Prénom" type="text" id="prenom" placeholder="Votre prénom" />
           </div>
-          <div className="create-account__register__title2">
+          <div className="create-account__form__inputsContainer__input">
+            <CgProfile className="inputIcon" />
+            <input name="Nom" type="text" id="nom" placeholder="Votre nom" />
+          </div>
+          <div className="create-account__form__inputsContainer__input">
+            <MdOutlineEmail className="inputIcon" />
+            <input name="email" type="email" id="email" placeholder="Votre email" />
+          </div>
+          <div className="create-account__form__inputsContainer__input">
+            <RiLockPasswordLine className="inputIcon" />
             <input
-              className="create-account__register__input"
-              name="Prénom"
-              type="text"
-              id="prenom"
-              placeholder="John"
-            />
-          </div>
-        </div>
-        <div className="create-account__register">
-          <div className="create-account__register__title1">
-            <label htmlFor="Nom">Nom*</label>
-          </div>
-          <div className="create-account__register__title2">
-            <input
-              className="create-account__register__input"
-              name="Nom"
-              type="text"
-              id="nom"
-              placeholder="Doe"
-            />
-          </div>
-        </div>
-        <div className="create-account__register">
-          <div className="create-account__register__title1">
-            <label htmlFor="Email">Email*</label>
-          </div>
-          <div className="create-account__register__title2">
-            <input
-              className="create-account__register__input"
-              name="email"
-              type="email"
-              id="email"
-              placeholder="john.doe@live.fr"
-            />
-          </div>
-        </div>
-        <div className="create-account__register">
-          <div className="create-account__register__title1">
-            <label htmlFor="Mot de passe">Mot de passe*</label>
-          </div>
-          <div className="create-account__register__title2">
-            <input
-              className="create-account__register__input"
               name="mot de passe"
-              type="password"
+              type={`${hiEye ? 'password' : 'text'}`}
               id="password"
-              placeholder="**********"
+              placeholder="Mot de passe"
             />
+            <HiEye className="inputIcon right" onClick={() => setHiEye(!hiEye)} />
           </div>
-        </div>
-        <div className="create-account__register">
-          <div className="create-account__register__title1">
-            <label htmlFor="Confirmation mot de passe">Confirmation mot de passe*</label>
-          </div>
-          <div className="create-account__register__title2">
+          <div className="create-account__form__inputsContainer__input">
+            <ImKey className="inputIcon" />
             <input
-              className="create-account__register__input"
               name="confirmation mot de passe"
-              type="password"
+              type={`${hiEye ? 'password' : 'text'}`}
               id="password"
-              placeholder="**********"
+              placeholder="Confirmation mot de passe"
             />
+            <HiEye className="inputIcon right" onClick={() => setHiEye(!hiEye)} />
           </div>
         </div>
-      </div>
-      <div className="create-account__right"></div>
-      <div className="create-account__register">
-        <div className="create-account__register__title1">
-          <label htmlFor="Date de naissance">Date de naissance*</label>
+      </form>
+      <div className="btn__container">
+        <h3 className="btn__container__title">Profil</h3>
+        <div className="btn__container__profil">
+          <input type="radio" id="" name="profil" checked />
+          <label htmlFor="radio-one">Particulier</label>
         </div>
-        <div className="create-account__register__title2">
-          <input
-            className="create-account__register__input"
-            name="date de naissance"
-            type="date"
-            id="date-of-birth"
-            placeholder="01/01/1980"
-          />
+        <div className="btn__container__profil">
+          <input type="radio" id="" name="profil" />
+          Professionnel
         </div>
       </div>
-      <div className="create-account__register">
-        <div className="create-account__register__title1">
-          <label htmlFor="Numéro de téléphone">Numéro de téléphone*</label>
+      <div className="btn__container">
+        <h3 className="btn__container__title">Genre</h3>
+        <div className="btn__container__genre">
+          <input type="radio" id="" name="genre" /> Homme
         </div>
-        <div className="create-account__register__title2">
-          <input
-            className="create-account__register__input"
-            name="numéro de téléphone"
-            type="tel"
-            id="phone"
-            placeholder="06 00 00 00 00"
-          />
+        <div className="btn__container__genre">
+          <input type="radio" id="" name="genre" /> Femme
+        </div>
+        <div className="btn__container__genre">
+          <input type="radio" id="" name="genre" /> Neutre
         </div>
       </div>
-      <div className="create-account__register">
-        <div className="create-account__register__title1">
-          <label htmlFor="Adresse">Adresse*</label>
-        </div>
-        <div className="create-account__register__title2">
-          <input
-            className="create-account__register__input"
-            name="adresse"
-            type="text"
-            id="adresse"
-            placeholder="5 Rue Marcel Pagnol"
-          />
-        </div>
+      <div className="checkboxCgv__createAccount">
+        <input className="checkboxCgv__createAccount__input" type="checkbox" />
+        <label className="checkboxCgv__createAccount__label">
+          {' '}
+          En créant votre compte vous acceptez notre{' '}
+          <a href="#!">politique de confidentialité</a>
+        </label>
       </div>
-      <div className="create-account__register">
-        <div className="create-account__register__title1">
-          <label htmlFor="complement-d-adresse">Complément d&rsquo;adresse</label>
-        </div>
-        <div className="create-account__register__title2">
-          <input
-            className="create-account__register__input"
-            name="complément d'adresse"
-            type="text"
-            id="complement-adresse"
-            placeholder="Complément d'adresse"
-          />
-        </div>
-      </div>
-      <div className="create-account__register">
-        <div className="create-account__register__title1">
-          <label htmlFor="code postal">Code postal*</label>
-        </div>
-        <div className="create-account__register__title2">
-          <input
-            className="create-account__register__input"
-            name="code postal"
-            type="text"
-            id="code-postal"
-            placeholder="13012"
-          />
-        </div>
-      </div>
-      <div className="create-account__register">
-        <div className="create-account__register__title1">
-          <label htmlFor="Ville">Ville*</label>
-        </div>
-        <div className="create-account__register__title2">
-          <input
-            className="create-account__register__input"
-            name="ville"
-            type="text"
-            id="ville"
-            placeholder="Marseille"
-          />
-        </div>
-      </div>
-      <button type="submit" className="btn createAccount__form__submit">
+      <button type="submit" className="btn__createAccount">
         S&rsquo;inscrire
       </button>
+      <div className="containerSignin">
+        <p>
+          Vous avez déjà un compte? <a href="/Connection">Connectez-vous</a>
+        </p>
+      </div>
     </div>
   );
 };
