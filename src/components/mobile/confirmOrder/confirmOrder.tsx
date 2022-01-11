ad43065b83a06ed62abeb0a7b77282d9b10352f7
ValentinLagues <valentin.lagues@gmail.com>
2022-01-11 15:11:14 +0100

    context en cours

---
+++ src/components/mobile/confirmOrder/confirmOrder.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const ConfirmOrder = () => {
+  return (
+    <div className="confirmorder">
+      <div className="confirmorder__Container">
+        <div>
+          <h3>Commande</h3>
+        </div>
+        <div>
+          <h3>Vos Coordonnées</h3>
+        </div>
+        <div>
+          <h3>Options de livraison</h3>
+        </div>
+        <div>
+          <h3>Résumé de la commande</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmOrder;
