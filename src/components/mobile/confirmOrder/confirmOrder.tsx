aef1eff450bfc97f2e06c95650617db6aeff3329
ValentinLagues <valentin.lagues@gmail.com>
2022-01-18 10:36:26 +0100

    component en cours

--- src/components/mobile/confirmOrder/confirmOrder.tsx
+++ src/components/mobile/confirmOrder/confirmOrder.tsx
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import React, { useContext, useEffect, useState } from 'react';
-import IOffers from '../../../interfaces/IOffer';
+import IOffer from '../../../interfaces/IOffer';
 import IUserLog from '../../../interfaces/IUser';
 import OfferContext from '../../../contexts/Offer';
 // import { BiPackage, BiCheckShield } from 'react-icons/bi';
 import { AiOutlineLock } from 'react-icons/ai';
+import CurrentUserContext from '../../../contexts/CurrentUser';
 
 interface IOffer_Deliverer {
   id_offer_deliverer: number;
@@ -15,22 +16,23 @@ interface IOffer_Deliverer {
 const urlBack = import.meta.env.VITE_URL_BACK;
 
 const ConfirmOrder = () => {
-  const { id } = useContext(OfferContext);
-  const [confirmOrder, setConfirmOrder] = useState<IOffers[]>([]);
-  const [confirmAdress, setConfirmAdress] = useState<IUserLog[]>([]);
-  const [confirmDeliverer, setConfirmDeliverer] = useState<IOffer_Deliverer[]>([]);
+  // const { id_offer } = useContext(OfferContext);
+  const { id } = useContext(CurrentUserContext);
+  const [confirmOrder, setConfirmOrder] = useState<IOffer>();
+  const [confirmAdress, setConfirmAdress] = useState<IUserLog>();
+  const [confirmDeliverer, setConfirmDeliverer] = useState<IOffer_Deliverer>();
   const [order, setOrder] = useState('');
   const [adress, setAdress] = useState('');
   const [deliverer, setDeliverer] = useState('');
 
   useEffect(() => {
-    axios.get(`${urlBack}/offers/${id}`).then((res) => setConfirmOrder(res.data)),
-      axios.get(`${urlBack}/users`).then((res) => setConfirmAdress(res.data)),
-      axios
-        .get(`${urlBack}/offer_deliverers`)
-        .then((res) => setConfirmDeliverer(res.data));
+    axios.get(`${urlBack}/offers/1`).then((res) => setConfirmOrder(res.data)),
+      axios.get(`${urlBack}/users/${id}`).then((res) => setConfirmAdress(res.data));
+    // axios
+    //   .get(`${urlBack}/offer_deliverers`)
+    //   .then((res) => setConfirmDeliverer(res.data));
   }, []);
-
+  console.log(1);
   return (
     <div className="confirmorder">
       <div className="confirmorder__Container">
@@ -47,20 +49,19 @@ const ConfirmOrder = () => {
         </div> */}
         <div>
           <h3>COMMANDE</h3>
-          {confirmOrder &&
-            confirmOrder.map((offer, index) => (
-              <div key={index}>
-                {offer.picture1}
-                {offer.title}
-                {offer.id_size}
-                {offer.id_condition}
-                {offer.price}
-              </div>
-            ))}
+          {confirmOrder && (
+            <div>
+              {/* {confirmOrder.picture1} */}
+              {confirmOrder.title}
+              {/* {offer.id_size}
+              {offer.id_condition}
+              {offer.price} */}
+            </div>
+          )}
         </div>
         <div>
           <h3>VOS COORDONNEES</h3>
-          {confirmAdress &&
+          {/* {confirmAdress &&
             confirmAdress.map((user, index) => (
               <div key={index}>
                 {user.firstname}
@@ -71,7 +72,7 @@ const ConfirmOrder = () => {
                 {user.city}
                 {user.country}
               </div>
-            ))}
+            ))} */}
         </div>
         <div>
           <h3>OPTIONS DE LIVRAISON</h3>
