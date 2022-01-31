e5372a96e337059cc0363cb56ad393720360695f
amandine <constans.amandine@gmail.com>
2022-01-31 15:57:10 +0100

    pastille color , hand delivery, deliverer it's ok

--- src/components/mobile/productDescription/ProductDescription.tsx
+++ src/components/mobile/productDescription/ProductDescription.tsx
@@ -8,23 +8,14 @@ import IBrand from '../../../interfaces/IBrand';
 import IColor from '../../../interfaces/IColor';
 import ICondition from '../../../interfaces/ICondition';
 import IDeliverer from '../../../interfaces/IDeliverer';
+// import IItem from '../../../interfaces/IItem';
 import Ioffer from '../../../interfaces/IOffer';
+import IOffer_deliverer from '../../../interfaces/IOffer_deliverer';
 import ISize from '../../../interfaces/ISize';
-// import ISport from '../../../interfaces/ISport';
 import IUserLog from '../../../interfaces/IUser';
 
 const urlBack = import.meta.env.VITE_URL_BACK;
 
-// interface IOffer {
-//   id_offer: number;
-// }
-
-// interface Color {
-//   id_color: number;
-//   name: string;
-//   color_code: string;
-//   style: object;
-// }
 interface Sport {
   id_sport: number;
   name: string;
@@ -37,15 +28,22 @@ const ProductDescription = () => {
   const [size, setSize] = useState<ISize>();
   const [condition, setCondition] = useState<ICondition>();
   const [sport, setSport] = useState<Sport>();
-  const [deliverer, setDeliverer] = useState<IDeliverer>();
-  const [color, setColor] = useState<IColor>();
-  // const [colorList, setColorList] = useState<Color[]>([]);
+  const [deliverer, setDeliverer] = useState<IDeliverer[]>([]);
+  const [offerDeliverer, setOfferDeliverer] = useState<IOffer_deliverer[]>([]);
+  const [handDeliverer, setHandDeliverer] = useState('');
+  const [color1, setColor1] = useState<IColor>();
+  const [color2, setColor2] = useState<IColor>();
+  // const [colorList, setColorList] = useState<IColor[]>([]);
   const [user, setUser] = useState<IUserLog>();
 
-  //   const [sellerAddress, setSellerAddress] = useState();
-  console.log(deliverer);
-
-  // const [idSport, setIdSport] = useState<Sport>();
+  // const [gender, setGender] = useState<number | null>(null);
+  // const [genderAdult, setGenderAdult] = useState<number | null>(null);
+  // const [genderChild, setGenderChild] = useState<number | null>(null);
+  // const [genderIsChild, setGenderIsChild] = useState(false);
+  // const [category, setCategory] = useState('');
+  // const [item, setItem] = useState('');
+  // const [itemInfos, setItemInfos] = useState<IItem>();
+  // const [categoryIsClothes, setCategoryIsClothes] = useState(false);
 
   const url = document.location.href;
   const id = url.substring(url.lastIndexOf('/') + 1);
@@ -54,41 +52,56 @@ const ProductDescription = () => {
 
   useEffect(() => {
     axios
+      .get(`${urlBack}/offers/${id}/offer_deliverers`)
+      .then((res) =>
+        setOfferDeliverer(res.data.map((deliverer: any) => deliverer.id_deliverer)),
+      );
+    // axios.get(`${urlBack}/colors`).then((res) => setColorList(res.data));
+
+    axios
       .get(`${urlBack}/offers/${id}`)
       .then((res) => res.data)
       .then((data) => {
         setOffer(data);
         // console.log = ok
         // axios.get(`${urlBack}/offers/1`).then((data) => console.log(data));
-        axios.get(`${urlBack}/brands/${data.id_brand}`).then((res) => setBrand(res.data));
-        axios.get(`${urlBack}/colors/${data.id_color}`).then((res) => setColor(res.data));
+
+        data.id_brand &&
+          axios
+            .get(`${urlBack}/brands/${data.id_brand}`)
+            .then((res) => setBrand(res.data));
+        data.id_color1 &&
+          axios
+            .get(`${urlBack}/colors/${data.id_color1}`)
+            .then((res) => setColor1(res.data));
+        data.id_color2 &&
+          axios
+            .get(`${urlBack}/colors/${data.id_color2}`)
+            .then((res) => setColor2(res.data));
         axios.get(`${urlBack}/sports/${data.id_sport}`).then((res) => setSport(res.data));
-        axios.get(`${urlBack}/sizes/${data.id_size}`).then((res) => setSize(res.data));
+        data.id_size &&
+          axios.get(`${urlBack}/sizes/${data.id_size}`).then((res) => setSize(res.data));
         axios
           .get(`${urlBack}/conditions/${data.id_condition}`)
           .then((res) => setCondition(res.data));
-        axios
-          .get(`${urlBack}/deliverers/${data.id_deliverer}`)
-          .then((res) => setDeliverer(res.data));
+        axios.get(`${urlBack}/deliverers`).then((res) => setDeliverer(res.data));
+
+        data.hand_delivery === 0
+          ? setHandDeliverer(
+              'La remise en main propre pour se produit n est pas disponible',
+            )
+          : setHandDeliverer('La remise en main propre pour se produit est disponible');
+
         axios
           .get(`${urlBack}/users/${data.id_user_seller}`, { withCredentials: true })
           .then((res) => setUser(res.data));
-        // axios.get(`${urlBack}/users/1/`);
       });
   }, []);
-  // console.log(offer)
+  color1 && (color1.style = { backgroundColor: color1?.color_code });
+  color2 && (color2.style = { backgroundColor: color2?.color_code });
 
-  // useEffect(() => {
-  //   offer && offer;
-  //   axios.get(`${urlBack}/sports/${idSport}`).then((res) => setIdSport(res.data));
-  // }, []);
-  // console.log(idSport);
-
-  // colorList &&
-  //   colorList.map((color) => (color.style = { backgroundColor: color.color_code }));
-
-  // if (size) {
-  // }
+  // console.log(color1);
+  // console.log(color2);
 
   return (
     <div className="product-description">
@@ -205,9 +218,6 @@ const ProductDescription = () => {
           <div className="product-description__container-text__container1__price">
             <span>{offer && offer.price} €</span>
           </div>
-          {/* <div className='product-description__container-text__container1__btn-buy'>
-                        <button>Acheter</button>
-                    </div> */}
         </div>
         <div className="product-description__container-text__container2__brand">
           <h2>{brand && brand.name}</h2>
@@ -225,10 +235,10 @@ const ProductDescription = () => {
           </div>
           <div className="product-description__container-text__container3__color">
             <h3>Couleurs</h3>
-            <p
-              className="product-description__container-text__container3__color__pastille-color"
-              {...(color && color.color_code)}
-              {...(color && color.style)}></p>
+            <p className="product-description__container-text__container3__color__pastille-color">
+              {color1 && <div className="colorPastille" style={color1.style}></div>}
+              {color2 && <div className="colorPastille" style={color2.style}></div>}
+            </p>
           </div>
           <div className="product-description__container-text__container3__sport-icon">
             <h3>Sport</h3>
@@ -247,27 +257,20 @@ const ProductDescription = () => {
             <h3>Description</h3>
             <p>{offer && offer.description}</p>
           </div>
-          {/* <div className="product-description__container-text__container4__btn-guide-size">
-            <button>Guide taille</button>
-          </div> */}
         </div>
         <div className="product-description__container-text__container4">
           <div className="product-description__container-text__container4__delivery">
             <h3>Livraison</h3>
-            <p>
-              Le produit est disponible en remise en main propre.
-              {offer && offer.hand_delivery}
-            </p>
+            <p>{handDeliverer && handDeliverer}</p>
+            <p>Secteur: {user && user.city}</p>
             <h4>Options d&apos;envoi</h4>
-            <button>
-              {/* {deliverer && deliverer.map(el, index)=> (
-              <option key={index} value={el.id_deliverer}>
-                {el.name}
-              </option> */}
-              {/* )} */}
-            </button>
-            {/* <button>La poste {offer && offer.colissimo_delivery}</button> */}
-            <p>{user && user.city}</p>
+            {offerDeliverer &&
+              deliverer &&
+              deliverer
+                .filter((allDeliverer) =>
+                  offerDeliverer.includes(Number(allDeliverer.id_deliverer)),
+                )
+                .map((delive, index) => <p key={index}>{delive.name}</p>)}
             <div className="product-description__container-text__container4__delivery__btn">
               <Link className="btn" type="submit" to="/confirmer-achat">
                 Acheter
