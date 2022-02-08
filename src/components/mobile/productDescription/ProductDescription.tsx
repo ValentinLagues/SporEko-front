a379c1fcbb05d33b4f9dea909f227b71068481cc
amandine <constans.amandine@gmail.com>
2022-02-08 13:26:49 +0100

    size on product description

--- src/components/mobile/productDescription/ProductDescription.tsx
+++ src/components/mobile/productDescription/ProductDescription.tsx
@@ -78,7 +78,7 @@ const ProductDescription = () => {
             setSport(res.data);
           });
         data.id_size &&
-          axios.get(`${urlBack}/sizes/${data.id_size}`).then((res) => setSize(res.data));
+          axios.get(`${urlBack}/sizes?id_size=${data.id_size}&id_item=${data.id_item}`).then((res) => setSize(res.data[0].size));
         axios
           .get(`${urlBack}/conditions/${data.id_condition}`)
           .then((res) => setCondition(res.data));
@@ -143,7 +143,7 @@ const ProductDescription = () => {
           <div className="product-description__container-text__container3__size">
             <h3>Taille</h3>
             <p>
-              {size && size.id_size} {size && size.size_eu}
+              {size && size}
             </p>
           </div>
           <div className="product-description__container-text__container3__condition">
